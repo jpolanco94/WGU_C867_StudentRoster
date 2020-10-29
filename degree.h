ae54fb38b5de70a22f81f9a7936b10fb7cd9175d
Jorge Polanco <Jlpolanco94@yahoo.com>
2020-10-28 20:26:42 -0400

    Files

--- degree.h
+++ degree.h
@@ -1,12 +1,10 @@
 #pragma once
-#include <iostream>
-using namespace std;
 
 enum DegreeProgram { SECURITY, NETWORK, SOFTWARE };
 
 /* Enums help throughout the program in assigning constant values to variables
 For real world applications I would have another value in the enum for students that
 may not have a set degree program. This helps for parameterless constructors and for 
-making sure thatthere are no null values in this program */
+making sure that there are no null values in this program */
 
-static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
